5b1b8db1f5123d28f43ae5c18bf33f655d084e99
Mark <marklajom.work@gmail.com>
2023-11-07 10:17:30 +0100

    Clear all function

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,8 @@ export default function App() {
 
 /* -----—-----—-----—-----—-----—-----—-----—-----—-----— */
 
-const initialTasks = [
-  { id: 124356, description: "Make the bed", completed: false },
-  { id: 7465736, description: "Wake up", completed: true },
-  { id: 4252023, description: "Eat breakfast", completed: false },
-];
-
 function ToDoList() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState([]);
 
   function handleAddTask(task) {
     setTasks([...tasks, task]);
@@ -27,12 +21,33 @@ function ToDoList() {
     setTasks((tasks) => tasks.filter((task) => task.id !== id));
   }
 
+  function handleToggleTask(id) {
+    setTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
+  function handleClearTasks() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete your tasks?"
+    );
+
+    if (confirmed) setTasks([]);
+  }
+
   return (
     <div className="border-2 border-gray-300 rounded-sm mx-auto my-7 w-96 flex justify-center items-center flex-col max-w-max pt-5 pb-3 px-3">
       <h1 className="text-4xl uppercase">to do list</h1>
       <Form onAddTask={handleAddTask} />
-      <TasksList tasks={tasks} onDeleteTask={handleDeleteTask} />
-      <Filter />
+      <TasksList
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onToggleTask={handleToggleTask}
+      />
+      <Filter tasks={tasks} />
+      {tasks.length === 0 || <ClearAll onClearTasks={handleClearTasks} />}
     </div>
   );
 }
@@ -71,30 +86,47 @@ function Form({ onAddTask }) {
   );
 }
 
-function TasksList({ tasks, onDeleteTask }) {
+function TasksList({ tasks, onDeleteTask, onToggleTask }) {
   return (
     <div className="p-1 w-full">
       {tasks.map((task) => (
-        <Task task={task} key={task.id} onDeleteTask={onDeleteTask} />
+        <Task
+          task={task}
+          key={task.id}
+          onDeleteTask={onDeleteTask}
+          onToggleTask={onToggleTask}
+        />
       ))}
     </div>
   );
 }
 
-function Task({ task, onDeleteTask }) {
+function Task({ task, onDeleteTask, onToggleTask }) {
   return (
     <div className="flex border-b border-gray-200 p-1 my-1 justify-between gap-3">
-      <input type="checkbox"></input>
+      <input
+        type="checkbox"
+        value={task.completed}
+        onChange={() => onToggleTask(task.id)}
+      />
       <p className="flex-1 first-letter:uppercase">{task.description}</p>
       <button onClick={() => onDeleteTask(task.id)}>❌</button>
     </div>
   );
 }
 
-function Filter() {
+function Filter({ tasks }) {
+  const taskNum = tasks.length;
+  const taskCompleted = tasks.filter((task) => task.completed).length;
+  const taskPercentage = Math.round((taskCompleted * 100) / taskNum);
+
   return (
     <div className="flex justify-between w-full relative bg-slate-100 p-1">
-      <p>3 items left</p>
+      <p>
+        {taskNum === 0
+          ? "Add a new task"
+          : `${taskNum} items (%${taskPercentage} completed)`}
+      </p>
       <label className="flex-1 text-right pr-1">filter:</label>
       <select name="" id="" className="bg-transparent">
         <option value="all">all</option>
@@ -102,5 +134,13 @@ function Filter() {
         <option value="completed">completed</option>
       </select>
     </div>
+  );
+}
+
+function ClearAll({ onClearTasks }) {
+  return (
+    <button className="mt-2 text-sm underline" onClick={() => onClearTasks()}>
+      Clear all
+    </button>
   );
 }
